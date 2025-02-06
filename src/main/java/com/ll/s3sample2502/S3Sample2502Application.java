5f8a03e449317561c1bc0e2720d13fc8357dc39f
sik2 <bickwang123@gmail.com>
2025-02-06 17:36:22 +0900

    init

---
+++ src/main/java/com/ll/s3sample2502/S3Sample2502Application.java
@@ -0,0 +1,13 @@
+package com.ll.s3sample2502;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3Sample2502Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3Sample2502Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/s3sample2502/S3Sample2502ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.s3sample2502;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3Sample2502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
